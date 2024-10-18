dedb2cf8865bb4426ed03883d3dd074db7fe4065
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-10-18 23:32:28 +1100

    Tidy up imports in `secret.tsx`.

--- routes/secret.tsx
+++ routes/secret.tsx
@@ -1,7 +1,7 @@
 import type { FreshContext } from "$fresh/server.ts";
 
-import { isAuthenticated } from "../utils/authentication.ts";
-import { createSignInRedirectResponse } from "../utils/redirect.ts";
+import { isAuthenticated } from "@/utils/authentication.ts";
+import { createSignInRedirectResponse } from "@/utils/redirect.ts";
 
 export default async function Secret(req: Request, ctx: FreshContext) {
     const userIsAuthenticated = isAuthenticated(ctx);
