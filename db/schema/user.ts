0e31bb7253d1d912b9de4b85c3eb5440027a9a2f
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-10-18 17:45:41 +1100

    Add migration for the `user` table.

---
+++ db/schema/user.ts
@@ -0,0 +1,50 @@
+import { sql } from "drizzle-orm";
+import {
+    check,
+    pgEnum,
+    pgTable,
+    timestamp,
+    uuid,
+    varchar,
+} from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+export type InsertUserSchema = z.infer<typeof insertUserSchema>;
+export type SelectUserSchema = z.infer<typeof selectUserSchema>;
+
+export enum OAuthProvider {
+    GITHUB = "github",
+}
+
+export const oauthProviderEnum = pgEnum(
+    "oauth_provider",
+    Object.values(OAuthProvider) as [OAuthProvider, ...OAuthProvider[]],
+);
+
+export const userTable = pgTable("user", {
+    id: uuid().defaultRandom().primaryKey(),
+    created: timestamp().defaultNow().notNull(),
+    updated: timestamp().defaultNow().notNull().$onUpdate(() =>
+        sql`CURRENT_TIMESTAMP`
+    ),
+    oauthId: varchar({ length: 255 }).notNull(),
+    oauthProvider: oauthProviderEnum().notNull(),
+    username: varchar({ length: 32 }).unique().notNull(),
+    displayName: varchar({ length: 255 }),
+    email: varchar({ length: 255 }).unique(),
+}, (table) => ({
+    checkConstraint: check(
+        "username_check",
+        sql`LENGTH(${table.username}) > 2`,
+    ),
+}));
+
+export const insertUserSchema = createInsertSchema(userTable, {
+    email: z.string().email().optional(),
+}).omit({
+    id: true,
+    created: true,
+});
+
+export const selectUserSchema = createSelectSchema(userTable);
