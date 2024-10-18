fbb47eada6f0d46ef80a0a56ab313e10f48f3f93
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-10-18 13:40:43 +1100

    Clean up unused page.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,6 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $_middleware from "./routes/_middleware.ts";
-import * as $api_joke from "./routes/api/joke.ts";
 import * as $index from "./routes/index.tsx";
 import * as $secret from "./routes/secret.tsx";
 import * as $sign_in from "./routes/sign-in.tsx";
@@ -17,7 +16,6 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/_middleware.ts": $_middleware,
-    "./routes/api/joke.ts": $api_joke,
     "./routes/index.tsx": $index,
     "./routes/secret.tsx": $secret,
     "./routes/sign-in.tsx": $sign_in,
