b59071a205eb98515a59864a44d46346952a8624
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-11-08 12:47:34 +1100

    Update `/api/posts/import` to ensure that we don't go over the 64 KB Deno KV limit.

--- routes/shikwasa/import.tsx
+++ routes/shikwasa/import.tsx
@@ -1,7 +1,7 @@
 import { Handlers, type RouteContext } from "$fresh/server.ts";
 
-import { createPost } from "@/db/repository/post.ts";
-import type { Post } from "@/db/schema/post.ts";
+import { createPost, createStaticPost } from "@/db/repository/post.ts";
+import { type Post, PostSchema } from "@/db/schema/post.ts";
 import { getRequiredFormValue } from "@/utils/form.ts";
 import { marked, sanitizeHtml } from "@/utils/input-processing.ts";
 
@@ -16,11 +16,17 @@ export const handler: Handlers = {
 			const unsanitizedContentHtml = await marked.parse(
 				entry.contentMarkdown,
 			);
-			const contentHtml = sanitizeHtml(unsanitizedContentHtml);
 
-			entry.contentHtml = contentHtml;
+			entry.contentHtml = sanitizeHtml(unsanitizedContentHtml);
 
-			await createPost(entry);
+			const byteSize = new Blob([JSON.stringify(entry)]).size;
+			const withinDenoKvSizeLimit = byteSize < 64E3;
+
+			if (withinDenoKvSizeLimit) {
+				await createPost(entry);
+			} else {
+				await createStaticPost(PostSchema.parse(entry));
+			}
 		}
 
 		const headers = new Headers({ "Location": "/shikwasa" });
